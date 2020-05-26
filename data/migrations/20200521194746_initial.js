3bcbcdb11448c11461ee1c5b4a370bfd5809a97c
Virginia Wilcox <wilcoxva@gmail.com>
2020-05-26 13:57:49 -0400

    fixed migrations to add plant foreign key and image

--- data/migrations/20200521194746_initial.js
+++ data/migrations/20200521194746_initial.js
@@ -1,20 +1,28 @@
-exports.up = async function(knex) {
-	await knex.schema.createTable("users", (table) => {
-		table.increments()
-		table.text("username").notNull().unique()
+exports.up = async function (knex) {
+    await knex.schema.createTable("users", (table) => {
+        table.increments()
+        table.text("username").notNull().unique()
         table.text("password").notNull()
         table.text("phoneNumber").notNull().unique()
     })
-    
+
     await knex.schema.createTable("plants", (table) => {
-		table.increments()
-		table.text("nickname").notNull().unique()
+        table.increments()
+        table.text("nickname").notNull().unique()
         table.text("species").notNull()
         table.integer("h2oFrequency").notNull()
+        table.string("image_url")
+        table
+            .integer("user_id")
+            .notNullable()
+            .references("id")
+            .inTable("users")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
     })
 }
 
-exports.down = async function(knex) {
+exports.down = async function (knex) {
     await knex.schema.dropTableIfExists("plants")
     await knex.schema.dropTableIfExists("users")
 }
