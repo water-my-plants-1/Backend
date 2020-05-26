76c9ab6bee268c5ccce8c6bca9a51cb7b836c7e1
Virginia Wilcox <wilcoxva@gmail.com>
2020-05-25 20:21:00 -0400

    added pg

--- data/config.js
+++ data/config.js
@@ -1,4 +1,9 @@
 const knex = require("knex")
 const knexfile = require("../knexfile")
 
-module.exports = knex(knexfile)
+// const dbenv = process.env.DB_ENV || "development";
+const dbenv = process.env.DEVELOPMENT || "production";
+console.log(dbenv)
+
+
+module.exports = knex(knexfile[dbenv])
--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,34 @@
 module.exports = {
-	client: "sqlite3",
-	useNullAsDefault: true,
-	connection: {
-		filename: "./data/auth.db3",
-	},
-	migrations: {
-		directory: "./data/migrations",
-	},
-	seeds: {
-		directory: "./data/seeds",
-	},
-	pool: {
-		afterCreate: (conn, done) => {
-			conn.run("PRAGMA foreign_keys = ON", done)
+	development: {
+		client: "sqlite3",
+		useNullAsDefault: true,
+		connection: {
+			filename: "./data/auth.db3",
+		},
+		migrations: {
+			directory: "./data/migrations",
+		},
+		seeds: {
+			directory: "./data/seeds",
+		},
+		pool: {      
+			afterCreate: (conn, done) => {             
+				conn.run('PRAGMA foreign_keys = ON', done);  
+			}
 		},
 	},
+	production: {
+		client: "pg",
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			directory: "./data/migrations",
+		},
+		seeds: {
+			directory: "./data/seeds",
+		},
+		pool: {      
+			min: 2,      
+			max: 10    
+		}
+	}
 }
