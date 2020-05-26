750f50651b731526f95e7b568232d8fe41712bab
Virginia Wilcox <wilcoxva@gmail.com>
2020-05-26 14:31:20 -0400

    fixed foreign key constraint in users seed

--- data/seeds/00_users.js
+++ data/seeds/00_users.js
@@ -2,8 +2,6 @@ const bcrypt = require("bcryptjs")
 const hash = async (password) => await bcrypt.hash(password, 12)
 
 exports.seed = async function (knex) {
-    await knex("users").truncate()
-
     await knex("users").insert([
         { id: 1, username: "username1", password: `${await hash('password')}`, phoneNumber: "7039226336" },
         { id: 2, username: "username2", password: `${await hash('password')}`, phoneNumber: "7576460791" },
