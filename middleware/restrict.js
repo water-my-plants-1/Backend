b52c0fd81a9d8a3d6657dbc83f4fbf0dee4e4d46
Virginia Wilcox <wilcoxva@gmail.com>
2020-05-27 22:27:04 -0400

    fixed restrict middleware2

--- middleware/restrict.js
+++ middleware/restrict.js
@@ -6,21 +6,6 @@ function restrict(role = "normal") {
 			message: "Invalid credentials",
 		}
 
-		const { authorization } = req.headers;
-
-		if (authorization) {
-			jwt.verify(authorization, jwtSecret, (err, decodedToken) => {
-				if (err) {
-					res.status(401).json({ message: "Invalid Credentials" })
-				} else {
-					req.decodedToken = decodedToken;
-					next;
-				}
-			});
-		} else {
-			res.status(400).json({ message: "No credentials provided" });
-		}
-
 		try {
 			if (!req.session || !req.session.user) {
 				return res.status(401).json(authError)
