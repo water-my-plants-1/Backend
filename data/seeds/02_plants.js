a3ae0fdfa8d082aae318a7a6bc820a212a94c879
Virginia Wilcox <wilcoxva@gmail.com>
2020-10-04 17:45:01 -0400

    added ids to seed

--- data/seeds/02_plants.js
+++ data/seeds/02_plants.js
@@ -1,9 +1,9 @@
 exports.seed = async function(knex) {
 
   await knex("plants").insert([
-  { id: 1, nickname: "baby", species: "lily", h2oFrequency: "4" },
-    { id: 2, nickname: "sunshine", species: "daffodil", h2oFrequency: "3" },
-    { id: 3, nickname: "duck", species: "daisy", h2oFrequency: "7" },
-    { id: 4, nickname: "hombre", species: "cactus", h2oFrequency: "1" },
+  { id: 1, nickname: "baby", species: "lily", h2oFrequency: "4", user_id: 4 },
+    { id: 2, nickname: "sunshine", species: "daffodil", h2oFrequency: "3", user_id: 1 },
+    { id: 3, nickname: "duck", species: "daisy", h2oFrequency: "7", user_id: 4 },
+    { id: 4, nickname: "hombre", species: "cactus", h2oFrequency: "1", user_id: 2 },
   ])
 };  
