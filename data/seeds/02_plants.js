2886d4f7645c0ef92731d0736d9a75fec258ffb1
Virginia Wilcox <wilcoxva@gmail.com>
2020-11-17 16:31:00 -0500

    updated seed file names

--- data/seeds/02_plants.js
+++ data/seeds/02_plants.js
@@ -1,14 +1,14 @@
 exports.seed = async function(knex) {
 
   await knex("plants").insert([
-    { id: 1, nickname: "baby", common_name: "Lily", h2oFrequency: "4", user_id: 1 },
-    { id: 2, nickname: "sunshine", common_name: "Daffodil", h2oFrequency: "3", user_id: 1 },
-    { id: 3, nickname: "duck", common_name: "Daisy", h2oFrequency: "7", user_id: 1 },
-    { id: 4, nickname: "hombre", common_name: "Cactus", h2oFrequency: "1", user_id: 2 },
-    { id: 5, nickname: "baby", common_name: "Hibiscus", h2oFrequency: "4", user_id: 2 },
-    { id: 6, nickname: "sunshine", common_name: "Orchid", h2oFrequency: "3", user_id: 2 },
-    { id: 7, nickname: "duck", common_name: "Fern", h2oFrequency: "7", user_id: 3 },
-    { id: 8, nickname: "hombre", common_name: "Basil", h2oFrequency: "1", user_id: 3 },
-    { id: 8, nickname: "hombre", common_name: "Tomato", h2oFrequency: "1", user_id: 3 },
+    { id: 1, nickname: "Lillie", common_name: "Lily", h2oFrequency: "4", user_id: 1 },
+    { id: 2, nickname: "Sunshine", common_name: "Daffodil", h2oFrequency: "3", user_id: 1 },
+    { id: 3, nickname: "Duck", common_name: "Daisy", h2oFrequency: "7", user_id: 1 },
+    { id: 4, nickname: "Hombre", common_name: "Cactus", h2oFrequency: "1", user_id: 2 },
+    { id: 5, nickname: "Rosa", common_name: "Hibiscus", h2oFrequency: "4", user_id: 2 },
+    { id: 6, nickname: "Dancing Lady", common_name: "Orchid", h2oFrequency: "3", user_id: 2 },
+    { id: 7, nickname: "Tree", common_name: "Fern", h2oFrequency: "7", user_id: 3 },
+    { id: 8, nickname: "Uomo", common_name: "Basil", h2oFrequency: "1", user_id: 3 },
+    { id: 8, nickname: "Red", common_name: "Tomato", h2oFrequency: "1", user_id: 3 },
   ])
 };  
