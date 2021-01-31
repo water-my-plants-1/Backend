8eb6b6cc7ccf4762c2e3ac4335087b8b400b53b9
Virginia Wilcox <wilcoxva@gmail.com>
2021-01-31 17:58:18 -0500

    changed add in plants model to just plant

--- models/plants-model.js
+++ models/plants-model.js
@@ -5,7 +5,7 @@ const add = async (plant, user_id) => {
 	plant = { ...plant, user_id }
 	const pgReturn = process.env.NODE_ENV === "production" ? "id" : null
 	const [id] = await db("plants").insert(plant, pgReturn)
-	return findById(id, user_id)
+	return findById(await db("plants").insert(plant))
 }
 
 function find(id) {
