2298026eee5e562dfc70f2e52e8dd3a5c497b3c9
Virginia Wilcox <wilcoxva@gmail.com>
2021-05-01 15:13:58 -0400

    added allow headers

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ server.use(cookieParser())
 
 server.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
 server.options('*', cors())
