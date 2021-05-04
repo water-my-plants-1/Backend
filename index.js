cce7534abd28ed3a084536a6cca181191822b129
Virginia Wilcox <wilcoxva@gmail.com>
2021-05-04 13:24:06 -0400

    installed bodyparser

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-require("dotenv").config()
-const express = require("express")
-const helmet = require("helmet")
-const cors = require("cors")
-const cookieParser = require("cookie-parser")
-const usersRouter = require("./routes/users-router")
-const plantsRouter = require("./routes/plants-router")
-const authRouter = require("./routes/auth-router")
-// const remindersRouter = require("./routes/reminders-router")
-const restrict = require("./middleware/restrict")
+require("dotenv").config();
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser');
+const usersRouter = require("./routes/users-router");
+const plantsRouter = require("./routes/plants-router");
+const authRouter = require("./routes/auth-router");
+// const remindersRouter = require("./routes/reminders-router");
+const restrict = require("./middleware/restrict");
 
-const server = express()
+const server = express();
 
-server.use(cors())
-server.use(helmet())
-server.use(express.json())
-server.use(cookieParser())
+server.use(cors());
+server.options('*', cors());
+server.use(bodyParser.json());
+server.use(helmet());
+server.use(express.json());
+server.use(cookieParser());
 
 server.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -22,17 +25,17 @@ server.use(function(req, res, next) {
 	next();
 });
 
-server.use("/", authRouter)
-server.use("/", restrict(), usersRouter)
-server.use("/user", restrict(), plantsRouter)
-// server.use("/user/plant", restrict(), remindersRouter)
+server.use("/", authRouter);
+server.use("/", restrict(), usersRouter);
+server.use("/user", restrict(), plantsRouter);
+// server.use("/user/plant", restrict(), remindersRouter);
 
 server.use((err, req, res, next) => {
 	res.status(500).json({
 		message: "Something went wrong",
-	})
-})
+	});
+});
 
-server.listen(process.env.PORT || 5000)
+server.listen(process.env.PORT || 5000);
 
 module.exports = server
