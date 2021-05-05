2592f6d08312b58499c44e7b6ff4dd61d51fe345
Virginia Wilcox <wilcoxva@gmail.com>
2021-05-05 11:59:39 -0400

    added authorization to headers

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ server.use(cookieParser());
 
 server.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "https://water-my-plants-frontend-wilcoxva.vercel.app");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 	res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
 	res.header("Access-Control-Allow-Credentials", true);
 	next();
